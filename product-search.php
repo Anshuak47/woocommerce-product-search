64c854eba2eab6f88614022e92909fbbf9722aed
Anshu <kushwaha.anshu8@gmail.com>
2025-08-20 00:11:23 +0530

    deployment script created

---
+++ product-search.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Product Search
+ * Plugin URI: https://https://journeywebsites.com/product-search
+ * Description: A plugin to enable product search functionality.
+ * Version: 1.0.0
+ * Author: Journey Websites
+ * Author URI: https://journeywebsites.com
+ * License:
+ */
+add_action("init","pres_product_search_shortcode", 10,1);
+function pres_product_search_shortcode(){
+    add_shortcode("product-search","pres_product_search_callback");
+}
+
+function pres_product_search_callback( $atts ){
+    // $atts = shortcode_atts(
+    //     array(
+    //         'id' => '',
+    //         'category' => array('categories'),
+    //     ),
+    //     $atts,
+    //     'product-search'
+    // );
+    echo 'Hello';
+}
